b917e95f719d0bba96e91bdbd7c6b9d533d324ba
olgavrou <olgavrou@gmail.com>
2023-11-02 21:55:26 -0400

    lock learn to pick version

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setuptools.setup(
         "mathchat": ["sympy", "pydantic==1.10.9", "wolframalpha"],
         "retrievechat": ["chromadb", "tiktoken", "sentence_transformers", "pypdf", "ipython"],
         "teachable": ["chromadb"],
-        "learn_to_pick": ["learn-to-pick"],
+        "learn_to_pick": ["learn-to-pick==0.0.3"],
     },
     classifiers=[
         "Programming Language :: Python :: 3",
